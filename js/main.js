e2da7fc56a2bd064dfe54b6b735c1d63c016b27c
s3w3nofficial <s3w3nofficial@gmail.com>
2019-12-07 18:41:56 +0000

    Deploying to master from dev 3f3f63b06b92cf74d42f45ec65e8b34cc2a87224

---
+++ js/main.js
@@ -0,0 +1,28 @@
+﻿var db = firebase.firestore()
+
+function addProject() {
+    db.collection("projects").add({
+        image: 'https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+        description: 'test description',
+        name: 'test'
+    })
+}
+
+async function getAllProjects() {
+    return (await db.collection("projects").get()).docs.map(doc => {
+        let data = doc.data();
+        return { name: data.name, image: data.image, description: data.description }
+    })
+}
+
+async function logIn() {
+    let provider = new firebase.auth.GithubAuthProvider();
+    return ((user) => ({
+        email: user['email'],
+        name: user['displayName']
+    }))((await firebase.auth().signInWithPopup(provider)).user);
+}
+
+async function logOut() {
+    await firebase.auth().signOut()
+}
